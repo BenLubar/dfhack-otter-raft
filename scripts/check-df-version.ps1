d2a358d85a6ede9899210b6728988535ffd64251
Administrator <none@example.com>
2017-10-08 15:43:10 -0500

    Updated by Otter.

---
+++ scripts/check-df-version.ps1
@@ -0,0 +1,35 @@
+params($lastETag, $lastModified)
+
+if ($lastModified -eq '') {
+	$lastModified = [DateTime]::MinValue
+}
+
+try {
+	$req = Invoke-WebRequest http://bay12games.com/dwarves/older_versions.html -Headers @{"If-None-Match" = $lastETag; "If-Modified-Since" = $lastModified}
+} catch {
+	$req = $_.Exception.Response
+}
+
+if ($req.StatusCode -eq 'NotModified') {
+	$etag = $lastETag
+	$modified = $lastModified
+	return
+}
+
+$etag = $req.Headers['ETag']
+$modified = $req.Headers['Last-Modified']
+
+$latestVersion = ($req.ParsedHtml.getElementsByTagName("p") | where className -eq 'menu')[0]
+
+if ($latestVersion.firstChild.textContent -match '^DF ([0-9]+\.[0-9]+\.[0-9]+) \(([A-Z][a-z]+ [1-9][0-9]?, [2-9][0-9]{3,})\)$') {
+	$version = $matches[1];
+	$date = [DateTime]::Parse($matches[2]).ToShortDateString()
+} else {
+	Write-Error 'Could not parse version header: ' + latestVersion.firstChild.textContent;
+}
+
+$versions = $latestVersion.getElementsByTagName('a') | where innerText -ne 'Small' | where innerText -ne 'Legacy Windows'
+
+foreach ($v in $versions) {
+	Invoke-WebRequest ('http://bay12games.com/dwarves/' + $v.pathname) -OutFile $v.pathname
+}
