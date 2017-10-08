cc6844e60c827b1298a5093fe11dec1d95235a61
Administrator <none@example.com>
2017-10-08 17:06:11 -0500

    Updated by Otter.

--- scripts/check-df-version.ps1
+++ scripts/check-df-version.ps1
@@ -4,38 +4,56 @@ if ($lastModified -eq '') {
 	$lastModified = [DateTime]::MinValue
 }
 
-$etag = $lastETag
-$modified = $lastModified
-$version = ''
-$date = ''
-$versions = @()
+@{'If-None-Match' = $lastETag, 'If-Modified-Since' = $lastModified} | Export-Clixml input.xml
 
-try {
-	$global:req = Invoke-WebRequest http://bay12games.com/dwarves/older_versions.html -Headers @{"If-None-Match" = $lastETag; "If-Modified-Since" = $lastModified}
-} catch {
-	$global:req = $_.Exception.Response
+powershell -version 5 -command {
+	$input = Import-Clixml input.xml
+
+	$output = @{
+		'etag' = $input['If-None-Match']
+		'modified' = $input['If-Modified-Since']
+		'version' = ''
+		'date' = ''
+		'versions' = @()
+	}
+
+	try {
+		$req = Invoke-WebRequest http://bay12games.com/dwarves/older_versions.html -Headers $input
+	} catch [Net.WebException] {
+		$req = $_.Exception.Response
+	}
+
+	if ($req.StatusCode -eq 'NotModified') {
+		$output | Export-Clixml output.xml
+		exit
+	}
+
+	$output['etag'] = $req.Headers['ETag']
+	$output['modified'] = $req.Headers['Last-Modified']
+
+	$latestVersion = ($req.ParsedHtml.getElementsByTagName('p') | where className -eq 'menu')[0]
+
+	if ($latestVersion.firstChild.textContent -match '^DF ([0-9]+\.[0-9]+\.[0-9]+) \(([A-Z][a-z]+ [1-9][0-9]?, [2-9][0-9]{3,})\)$') {
+		$output['version'] = $matches[1];
+		$output['date'] = [DateTime]::Parse($matches[2]).ToShortDateString()
+	} else {
+		Write-Error 'Could not parse version header: ' + latestVersion.firstChild.textContent;
+	}
+
+	$output['versions'] = $latestVersion.getElementsByTagName('a') | where innerText -ne 'Small' | where innerText -ne 'Legacy Windows' | foreach-object pathname
+
+	foreach ($v in $output['versions']) {
+		Invoke-WebRequest ('http://bay12games.com/dwarves/' + $v) -OutFile $v
+	}
+
+	$output | Export-Clixml output.xml
+	exit
 }
 
-$req = $global:req
+$output = Import-Clixml output.xml
 
-if ($req.StatusCode -eq 'NotModified') {
-	return
-}
-
-$etag = $req.Headers.Item('ETag')
-$modified = $req.Headers.Item('Last-Modified')
-
-$latestVersion = ($req.ParsedHtml.getElementsByTagName('p') | where className -eq 'menu')[0]
-
-if ($latestVersion.firstChild.textContent -match '^DF ([0-9]+\.[0-9]+\.[0-9]+) \(([A-Z][a-z]+ [1-9][0-9]?, [2-9][0-9]{3,})\)$') {
-	$version = $matches[1];
-	$date = [DateTime]::Parse($matches[2]).ToShortDateString()
-} else {
-	Write-Error 'Could not parse version header: ' + latestVersion.firstChild.textContent;
-}
-
-$versions = $latestVersion.getElementsByTagName('a') | where innerText -ne 'Small' | where innerText -ne 'Legacy Windows'
-
-foreach ($v in $versions) {
-	Invoke-WebRequest ('http://bay12games.com/dwarves/' + $v.pathname) -OutFile $v.pathname
-}
+$etag = $output['etag']
+$modified = $output['modified']
+$version = $output['version']
+$date = $output['date']
+$versions = $output['versions']
