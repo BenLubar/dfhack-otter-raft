8c304b4c5ec7cd4fb3ae6f18360d963e174e3579
Ben Lubar <ben.lubar@gmail.com>
2018-08-20 09:03:37 -0500

    Modified DeploymentPlan and Script ×2 with BuildMaster
    Modified DeploymentPlan Publish DFHack Plugin
    Modified Script dfhack-macgcc-split-debug-info.sh
    Modified Script macgcc-split-debug-info.sh

--- scripts/dfhack-macgcc-split-debug-info.sh
+++ scripts/dfhack-macgcc-split-debug-info.sh
@@ -6,7 +6,7 @@ shift
 split_debug_info()
 {
     echo "Splitting debug info from $1"
-    docker run --rm -i -v "$BaseWorkingDirectory:$BaseWorkingDirectory" -u $(id -u):$(id -g) -w "$(pwd)" proget.local.lubar.me/dfhack-docker/build-env dsymutil "$1"
+    docker run --rm -i -v "$BaseWorkingDirectory:$BaseWorkingDirectory" -u $(id -u):$(id -g) -w "$(pwd)" proget.lubar.me/dfhack-docker/build-env dsymutil "$1"
     mkdir -p "$2"
     rm -rf "$2/$1.dSYM"
     mv "$1.dSYM" "$2/"
